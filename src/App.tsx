8fbb80d6adbd048d544924177155f3c2ba120f87
Scott Clingan <sclingan1@gmail.com>
2023-03-02 13:29:15 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <main>
+        
+      </main>
+ 
+    </div>
+  )
+}
+
+export default App
