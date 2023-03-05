6755471dca70a9ff6fe516c4e78e61ba911f1608
Scott Clingan <sclingan1@gmail.com>
2023-03-05 00:10:44 -0600

    Added assets and API data

--- src/aside.tsx
+++ src/aside.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import data from '../src/assets/data.json'
 // import all images
 import reaction from '../public/icon-reaction.svg'
 import memory from '../public/icon-memory.svg'
@@ -7,38 +8,41 @@ import verbal from '../public/icon-verbal.svg'
 import visual from '../public/icon-visual.svg'
 
 function Aside() {
-
+  const Reaction = data[0].score;
+  const Memory = data[1].score;
+  const Verbal = data[2].score;
+  const Visual = data[3].score;
   return (
    <aside>
     {/* make this a list label?? */}
         <h2>Summary</h2>
         <ul>
             <li>
-              <div className='div__reaction results'><img src={reaction}></img>
+              <div className='div__reaction results'><img src={reaction} alt=""></img>
                 <p>Reaction</p>
-                <span>80 / 100</span>
+                <span>{Reaction} / 100</span>
               </div>
             </li>
             <li>
-              <div className='div__memory results'><img src={memory}></img>
+              <div className='div__memory results'><img src={memory} alt=""></img>
                 <p>Memory</p>
-                <span>92 / 100</span>
+                <span>{Memory} / 100</span>
               </div>
             </li>
             <li>
-              <div className='div__verbal results'><img src={verbal}></img>
+              <div className='div__verbal results'><img src={verbal} alt=""></img>
                 <p>Verbal</p>
-                <span>61 / 100</span>
+                <span>{Verbal} / 100</span>
                 </div>
             </li>
             <li>
-              <div className='div__visual results'><img src={visual}></img>
+              <div className='div__visual results'><img src={visual} alt=""></img>
                 <p>Visual</p>
-                <span>72 / 100</span>
+                <span>{Visual} / 100</span>
                 </div>
             </li>
         </ul>
-          <a href="#">Continue</a>
+          <a className='button__continue' href="#">Continue</a>
    </aside>
   )
 }
