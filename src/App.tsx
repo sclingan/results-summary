df7d8b75c19c6c6f9ac572ffd740c3255a558cea
Scott Clingan <sclingan1@gmail.com>
2023-03-05 00:36:23 -0600

    Final adjustments for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <h2>Your Result</h2>
         <div className='div__score'>
           <p className='p__score'>76</p>
-          <p>of 100</p>
+          <p className='p__score_circle'>of 100</p>
         </div>
         <h3>Great</h3>
         <p className='p__score_results'>You scored higher than 65% of the people who have taken these tests.</p>
